abe03054b7d13d4d3dc798a5994b4020bdac3e2b
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-03-12 10:50:21 +0100

    Connection to database (context.xml and ConnectionProvider class

---
+++ src/pl/boniaszczuk/mylibrary/util/ConnectionProvider.java
@@ -0,0 +1,31 @@
+package pl.boniaszczuk.mylibrary.util;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+    private static DataSource dataSource;
+
+    public static Connection getConnection() throws SQLException{
+        return getDataSource().getConnection();
+    }
+
+    private static DataSource getDataSource() {
+        if(dataSource == null){
+            try {
+                Context initialContext = new InitialContext();
+                Context envContext = (Context) initialContext
+                        .lookup("java:comp/env");
+                DataSource ds = (DataSource) envContext.lookup("jdbc/library");
+                dataSource = ds;
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+        }
+        return dataSource;
+    }
+}
